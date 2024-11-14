b9fd72d8898dc293f71b1762c8a509f676d77842
Gorra.js <gorrahabla1234@gmail.com>
2024-11-14 11:27:01 -0300

    modificacion de homepage

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,65 +1,67 @@
-import Preguntas from './Preguntas';
-import './Principal.css';
-  import { useState } from 'react';
+import Preguntas from "./Preguntas";
+import "./Principal.css";
+import { useState } from "react";
 
 function HomePage() {
-  const categories = [
-    'Terror',
-    'Drama',
-    'Romance',
-    'Acción',
-    'Ciencia Ficción',
-    'Comedia',
-    'Aventura',
-    'Suspenso'
-  ];
+	const categories = [
+		"Terror",
+		"Drama",
+		"Romance",
+		"Acción",
+		"Ciencia Ficción",
+		"Comedia",
+		"Aventura",
+		"Suspenso",
+	];
 
-  const [isContentVisible, setIsContentVisible] = useState(false); // Estado para controlar la visibilidad del contenido
+	const [isContentVisible, setIsContentVisible] = useState(false);
 
-  const toggleContentVisibility = () => {
-    setIsContentVisible(prevState => !prevState); // Alterna la visibilidad del contenido
-  };
+	const toggleContentVisibility = () => {
+		setIsContentVisible((prevState) => !prevState);
+	};
 
-  const [startIndex, setStartIndex] = useState(0);
-  const itemsToShow = 3;
+	const [startIndex, setStartIndex] = useState(0);
+	const itemsToShow = 3;
 
-  const handleNext = () => {
-    setStartIndex((prevIndex) => (prevIndex + itemsToShow) % categories.length);
-  };
+	const handleNext = () => {
+		setStartIndex(
+			(prevIndex) => (prevIndex + itemsToShow) % categories.length
+		);
+	};
 
-  const handlePrev = () => {
-    setStartIndex((prevIndex) => (prevIndex - itemsToShow + categories.length) % categories.length);
-  };
-  const handleButtonClick = () => {
-    window.location.href = "/Preguntas"; 
-  };
+	const handlePrev = () => {
+		setStartIndex(
+			(prevIndex) =>
+				(prevIndex - itemsToShow + categories.length) %
+				categories.length
+		);
+	};
+	const handleButtonClick = () => {
+		window.location.href = "/Preguntas";
+	};
 
-  return (
-    <div className='homePage'>
-      <h1>Título</h1>
-      <p>Click on the Vite and React logos to learn more</p>
-      <p id='arrow0' className='arrows' onClick={toggleContentVisibility}>v</p>
-      
-      {isContentVisible && ( // Solo muestra el siguiente contenido si isContentVisible es true
-        <>
-          <div className='discoveryBegins'>
-            <h2>¿No sabes qué película ver?</h2>
-            <button id='discoveryButton'  onClick={handleButtonClick}>Inicia Descubrimiento</button>
-            <img src="/img/homePageTitle.png" alt="Home Page Title" />
-          </div>
-          <div className='certainCategories'>
-            <button className='arrows' onClick={handlePrev}>←</button>
-            <div className='categoryDisplay'>
-              {categories.slice(startIndex, startIndex + itemsToShow).map((category, index) => (
-                <button key={index} className='buttonsCategStart'>{category}</button>
-              ))}
-            </div>
-            <button className='arrows' onClick={handleNext}>→</button>
-          </div>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="homePage">
+			<h1 className="title">Welcome</h1>
+			<>
+				<div className="discoveryBegins">
+					<img src="/img/homePageTitle.png" alt="Home Page Title" />
+					<div className="leftSquare">
+						<center>
+
+            <h2>¿Don't know what movie to watch?</h2>
+						<button
+							id="discoveryButton"
+							onClick={handleButtonClick}
+              >
+							Start Discovery
+						</button>
+              </center>
+					</div>
+				</div>
+			</>
+		</div>
+	);
 }
 
 export default HomePage;
